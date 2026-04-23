c4fdd0e3b08dc898353290f0d9c0b94bc036cf84
Endo Shogo <sigesigeito@gmail.com>
2026-04-23 17:07:20 +0900

    add abbr

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -9,7 +9,15 @@ abbr -a -- ggpull 'git pull origin (git branch --show-current)'
 abbr -a -- ggpush 'git push -u origin (git branch --show-current)'
 abbr -a -- gsw 'git switch'
 abbr -a -- gswc 'git switch --create'
+abbr -a -- gfsw 'git fetch; git switch'
+abbr -a -- grhh git restore .
+abbr -a -- gres git restore .
+abbr -a -- rm rm -i
+abbr -a -- e eza -hal
+abbr -a -- copy wl-copy -n
+abbr -a -- paste wl-paste -n
+abbr -a -- ov -n
+abbr -a -- fbat 'bat (fzf)'
 abbr -a -- v vim
 abbr -a -- tail1 tail -n 1
 abbr -a -- head1 head -n 1
-
